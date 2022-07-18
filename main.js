bd7b0f25d466c99848d217c7d76cb39e88525964
davide <dacapoccia@gmail.com>
2022-07-18 15:10:09 +0200

    milestone0

---
+++ main.js
@@ -0,0 +1,40 @@
+/*NOME                  RUOLO                        IMMAGINE
+Wayne Barnett	      Founder & CEO	               wayne-barnett-founder-ceo.jpg
+Angela Caroll	      Chief Editor	               angela-caroll-chief-editor.jpg
+Walter Gordon	      Office Manager	           walter-gordon-office-manager.jpg
+Angela Lopez	      Social Media Manager	       angela-lopez-social-media-manager.jpg
+Scott Estrada	      Developer	                   scott-estrada-developer.jpg
+Barbara Ramos	      Graphic Designer	           barbara-ramos-graphic-designer.jpg */
+
+const ourTeam = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        pic: 'wayne-barnett-founder-ceo.jpg'
+    }
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        pic: 'angela-caroll-chief-editor.jpg'
+    }
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        pic: 'walter-gordon-office-manager.jpg'
+    }
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        pic: 'angela-lopez-social-media-manager.jpg'
+    }
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        pic: 'scott-estrada-developer.jpg'
+    }
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        pic: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
